e778e41b26c468904e2b587342e2feefa8bf7ba9
ravid goldenberg <37501933+ravidGoldenberg@users.noreply.github.com>
2023-03-30 16:12:59 +0300

    Update seeds.js

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -20,21 +20,97 @@ var User = mongoose.model("User");
 seed();
 
 function seed() {
+  preCleenAllData();
+  seedUsers();
+  seedItems();
+  seedComments();
+    
+}
 
-    User.findOne({ username: 'rg' }).then(function(user){ 
-        const items = generateItems(user);
-        Item.insertMany(items);
+function preCleenAllData() {
+  Comment.deleteMany({}).then();;
+  Item.deleteMany({}).then();
+  User.deleteMany({username: {$ne: "rg"}}).then();
+}
+
+function seedUsers() {
+  const users = generateUsers();
+  User.insertMany(users);
+}
+
+function generateUsers() {
+  const users = []
+
+  const username = `defUser`;
+  const email = `defUser@email.com`;
+  const bio = `defUser-bio`;
+  const image = `https://example.com/defUser.jpg`;
+
+  const user = new User({
+    username,
+    email,
+    bio,
+    image
+  });
+
+  users.push(user);
+
+  for (let i = 1; i <= 99; i++) {
+    const username = `usertemp${i}`;
+    const email = `usertemp${i}@email.com`;
+    const bio = `usertemp${i}-bio`;
+    const image = `https://example.com/usertemp.jpg`;
+    
+    const user = new User({
+      username,
+      email,
+      bio,
+      image
     });
+
+    users.push(user);
+  } 
+
+  return users;
+}
+
+function seedItems() {
+  User.findOne({ username: 'defUser' }).then(function(user) { 
+    const items = generateItems(user);
+    Item.insertMany(items);
+});
 }
 
 function generateItems(user) {
     const items = [];
   
-    for (let i = 1; i <= 100; i++) {
-      const slug = `item-${i}`;
-      const title = `Item ${i}`;
-      const description = `This is item ${i}`;
-      const image = `https://example.com/item-${i}.jpg`;
+    const slug = `defItem`;
+    const title = `defItem`;
+    const description = `This is defItem`;
+    const image = `https://example.com/defItem.jpg`;
+    const favoritesCount = Math.floor(Math.random() * 100);
+    const comments = [];
+    const tagList = ['tag1', 'tag2', 'tag3'];
+    const sellerId = user;
+  
+    const item = new Item({
+      slug,
+      title,
+      description,
+      image,
+      favoritesCount,
+      comments,
+      tagList,
+      seller: sellerId
+    });
+  
+    items.push(item);
+
+    for (let i = 1; i <= 99; i++) {
+      const slug = `item-temp-${i}`;
+      const title = `Item-temp-${i}`;
+      const description = `This is item-temp-${i}`;
+      const image = `https://example.com/item-temp-${i}.jpg`;
       const favoritesCount = Math.floor(Math.random() * 100);
       const comments = [];
       const tagList = ['tag1', 'tag2', 'tag3'];
@@ -55,4 +131,33 @@ function generateItems(user) {
     }
   
     return items;
+}
+
+function seedComments() {
+  User.findOne({ username: 'defUser' }).then(function(user) { 
+    Item.findOne({ slug: 'defItem' }).then(function(item) {
+      const comments = generateComments(user, item);
+      Comment.insertMany(comments);
+    });
+  });
+}
+
+function generateComments(user, tItem) {
+  const comments = [];
+  
+  for (let i = 1; i <= 100; i++) {
+    const body = `new comment ${i}`;
+    const item = tItem;
+    const seller = user;
+
+    var comment = new Comment({
+      body,
+      seller,
+      item
+    });
+
+    comments.push(comment);
   }
+
+  return comments;
+}
